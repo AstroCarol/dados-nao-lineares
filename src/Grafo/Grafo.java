055e9ab5f72ae62a247d1f49facbe041d4707687
AstroCarol <astro.carol.gil@gmail.com>
2019-10-15 14:42:21 -0300

    Atts

--- src/Grafo/Grafo.java
+++ src/Grafo/Grafo.java
@@ -1,6 +1,8 @@
 package Grafo;
 
 import java.util.Vector;
+import java.util.ArrayList;
+
 
 public class Grafo {
     private int qtdVertices;
@@ -11,8 +13,16 @@ public class Grafo {
         vertices = new Vector();
     }
 
-    public void inserirVertice(Vertice v){ }
+    public void inserirVertice(Vertice v){
+        vertices.add(v);
+    }
 
+    public void removerVertice(Vertice v){
+        vertices.remove(v);
+    }
 
+    public void inserirAresta(Vertice inicio, Vertice fim){
+
+    }
 
 }
---
+++ src/Grafo/Main.java
@@ -0,0 +1,10 @@
+package Grafo;
+
+public class Main {
+    public static void main(String args[]){
+        Grafo g = new Grafo();
+
+        g.inserirVertice(new Vertice("1", "v1"));
+        g.toString();
+    }
+}

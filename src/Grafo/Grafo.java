1e72188bc77728f1abdd7a32e1c6e1a091598f35
AstroCarol <astro.carol.gil@gmail.com>
2019-10-16 07:36:09 -0300

    mais atts

--- src/Grafo/Grafo.java
+++ src/Grafo/Grafo.java
@@ -44,6 +44,20 @@ public class Grafo {
         matrizAdj[indice1][indice2] = a;
     }
 
+    public void removerLaco(Vertice inicio, Vertice fim){
+        int indice1 = buscar(inicio.getChave());
+        int indice2 = buscar(fim.getChave());
+        matrizAdj[indice1][indice2] = null;
+    }
+
+    public void inserirArestaAdjacente(Vertice inicio, Vertice fim){
+
+    }
+
+    public void removerArestaAdjacente(Vertice inicio, Vertice fim){
+
+    }
+
     private int buscar(String chave){
         Iterator i = vertices.iterator();
         int ind =  0;
@@ -56,4 +70,18 @@ public class Grafo {
         return -1;
     }
 
+    public void mostrarVertices(){
+        for (int i = 0; i < vertices.size(); i++){
+            System.out.print(vertices.elementAt(i)+",");
+        }
+    }
+
+    public void mostrarMatriz(){
+        for (int i = 0; i < qtdVertices; i++){
+            for (int j = 0; j < qtdVertices; j++){
+                System.out.print(matrizAdj[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
 }
